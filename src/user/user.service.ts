a5d423b001c0abbfe7ff264c01a66ec03c823191
West <coolwest@gmail.com>
2020-03-22 11:29:10 -0500

    Add basic user service using in memory array

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,5 @@
-/*
-  TODO:
-    - Get one User
-    - Get all users if admin
-    - Create user
-    - Update user
-    - Delete user
-*/
-
 import { Injectable } from '@nestjs/common';
-
-export type User = any;
+import { User } from './user.model';
 
 @Injectable()
 export class UserService {
@@ -21,21 +11,54 @@ export class UserService {
         userId: 1,
         username: 'john',
         password: 'changeme',
+        role: 'admin'
       },
       {
         userId: 2,
         username: 'chris',
         password: 'secret',
+        role: 'basic'
       },
       {
         userId: 3,
         username: 'maria',
         password: 'guess',
+        role: 'basic'
       },
     ];
   }
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+  async getAll(): Promise<User[] | undefined> {
+    return this.users;
+  }
+
+  async getOne(id: string): Promise<User | undefined> {
+    return this.users.find( user => user.userId === parseInt(id) );
+  }
+
+  async getOneByUsername(username: string): Promise<User | undefined> {
+    return this.users.find( user => user.username === username );
+  }
+
+  async create(user: User): Promise<User | undefined> {
+    user.userId = User.getMaxId(this.users);
+    user.role = 'basic';
+    this.users.push(user);
+    return user;
+  }
+
+  async update(updatedUser: User): Promise<User | undefined> {
+    const oldUserData = this.users.find( user => user.userId === updatedUser.userId );
+    this.delete( oldUserData.userId.toString() );
+    this.users.push(updatedUser);
+    return updatedUser;
+  }
+
+  async deleteByUsername(username: string): Promise<void> {
+    this.users.filter( user => user.username !== username );
+  }
+
+  async delete(id: string): Promise<void> {
+    this.users.filter( user => user.userId !== parseInt(id) );
   }
 }
