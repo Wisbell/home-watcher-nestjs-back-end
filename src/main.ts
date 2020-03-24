5338496ee7aad4cf3e68a4dfe3660f4871393a5d
West <coolwest@gmail.com>
2020-03-23 20:32:06 -0500

    Add development config and use of config files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    // app.enableCors({ origin: serverConfig.origin });
+    throw new Error('Add code above');
+  }
+
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
