b7cc15fb8d58e768ecf79cc62533743777076822
West <coolwest@gmail.com>
2020-03-31 12:23:31 -0500

    Add image module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ImageModule } from './image/image.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     UserModule,
-    AuthModule
+    AuthModule,
+    ImageModule
   ],
   controllers: [AppController],
   providers: [AppService],
