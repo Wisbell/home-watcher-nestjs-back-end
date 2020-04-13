451076a4f74dc7027fe12a7671de712e9375d99f
West <coolwest@gmail.com>
2020-04-12 21:49:46 -0500

    Add seeding of images and users on load

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -2,16 +2,23 @@ import { Module } from '@nestjs/common';
 import { SeederService } from './seeder.service';
 import { SeederController } from './seeder.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from '../user/user.service';
+import { UserRepository } from '../user/user.repository';
+import { ImageService } from '../image/image.service';
+import { Image } from '../image/image.entity';
 
 @Module({
   imports: [
-    // TypeOrmModule.forFeature([KeeperRepository]),
+    TypeOrmModule.forFeature([UserRepository]),
+    TypeOrmModule.forFeature([Image])
   ],
   controllers: [
     SeederController
   ],
   providers: [
-    SeederService
+    SeederService,
+    UserService,
+    ImageService
   ]
 })
 export class SeederModule {}
--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -4,13 +4,14 @@ import { User } from '../user/user.entity';
 import { Image } from '../image/image.entity';
 import * as userData from './data/users.json';
 import * as imageData from './data/images.json';
+import { UserService } from '../user/user.service';
+import { ImageService } from '../image/image.service';
 
 @Injectable()
 export class SeederService {
   constructor(
-    // private keeperService: KeeperService,
-    // private trainerService: TrainerService,
-    // private animalService: AnimalService
+    private userService: UserService,
+    private imageService: ImageService
   ) {}
 
   seedUsers(): void {
@@ -34,6 +35,14 @@ export class SeederService {
     });
   }
 
+  async deleteUsers(): Promise<void> {
+    const allUsers = await this.userService.getAllUsers();
+
+    allUsers.forEach(async (user: User) => {
+      await this.userService.deleteUser(user.id.toString());
+    });
+  }
+
   seedImages(): void {
     imageData.forEach(async (imageJSON) => {
 
@@ -54,8 +63,58 @@ export class SeederService {
     });
   }
 
+  async deleteImages(): Promise<void> {
+    const allImages = await this.imageService.getAllImages();
+
+    allImages.forEach(async (image: Image) => {
+      await this.imageService.deleteImage(image.id.toString());
+    });
+  }
+
   seedIncidents(): void {
 
   }
 
+  private async seedAll(): Promise<void> {
+    await this.seedUsers();
+    await this.seedImages();
+  }
+
+  private async deleteAll(): Promise<void> {
+    await this.deleteUsers();
+    await this.deleteImages();
+  }
+
+  async resetDatabase(): Promise<void> {
+    await this.deleteAll();
+    await this.resetAllTableSequences();
+    await this.seedAll();
+  }
+
+  async resetAllTableSequences(): Promise<void> {
+    const tableNames = await this.getAllTableNames();
+    tableNames.forEach( async (name) => await this.resetTableSequence(name) );
+  }
+
+  async resetTableSequence(tableName: string): Promise<void> {
+    return await getConnection()
+      .createEntityManager()
+      .query(`ALTER SEQUENCE ${tableName}_id_seq RESTART WITH 1`);
+  }
+
+  async getAllTableNames(): Promise<string[]> {
+    const tableNamesFromDB = await getConnection()
+      .createEntityManager()
+      .query(`SELECT table_name
+              FROM information_schema.tables
+              WHERE table_schema='public'
+              AND table_type='BASE TABLE';`);
+
+    const tableNames = tableNamesFromDB.map( (obj) => {
+      if(obj.table_name != 'migration')
+        return obj.table_name;
+    });
+
+    return tableNames.filter( name => name != null );
+  }
 }
