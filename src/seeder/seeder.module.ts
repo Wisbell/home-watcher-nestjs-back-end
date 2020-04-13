d4261301e82b3428067ca38da2bb6a23fb7cdbfd
West <coolwest@gmail.com>
2020-04-13 15:46:51 -0500

    Add correct providers and imports

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -6,11 +6,14 @@ import { UserService } from '../user/user.service';
 import { UserRepository } from '../user/user.repository';
 import { ImageService } from '../image/image.service';
 import { Image } from '../image/image.entity';
+import { Incident } from '../incident/incident.entity';
+import { IncidentService } from '../incident/incident.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
-    TypeOrmModule.forFeature([Image])
+    TypeOrmModule.forFeature([Image]),
+    TypeOrmModule.forFeature([Incident])
   ],
   controllers: [
     SeederController
@@ -18,7 +21,8 @@ import { Image } from '../image/image.entity';
   providers: [
     SeederService,
     UserService,
-    ImageService
+    ImageService,
+    IncidentService
   ]
 })
 export class SeederModule {}
